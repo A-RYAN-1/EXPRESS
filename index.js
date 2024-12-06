13b1353847a5d68175f6ddeb611f2f2aecfb342a
Aryan Deshmukh <amazingaryan04@gmail.com>
2024-12-06 17:10:32 +0530

    Second Commit (06/12/2024)

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express')
+const path = require('path')
 const app = express()
-const port = 3000
+const port = 4000
 
-app.get('/', (req,res) => {
-    res.send(`We don't fit in well because we are just ourselves`)
+app.use(express.static(path.join(__dirname,"public")))
+
+app.get('/hello/:name', (req,res) => {
+    res.send(`We don't fit in well because we are just ourselves, right? ` + req.params.name)
+})
+
+app.get('/about', (req,res) => {
+    res.sendFile(path.join(__dirname, 'index.html'))
+    res.status(500)
 })
 
 app.listen(port, () => {
